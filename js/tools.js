a9240b0c07c40e6d644242bda145fe02b10eb536
Stefan Strigler <stefan@strigler.de>
2014-09-11 15:29:38 +0200

    logout onunload

--- js/tools.js
+++ js/tools.js
@@ -7,6 +7,7 @@ function Client(config) {
     this.con.registerHandler('onconnect', JSJaC.bind(this.connected, this));
 };
 
+// api
 Client.prototype.login = function(jid, pw) {
     var domain = jid.substring(jid.indexOf('@')+1);
     var username = jid.substring(0, jid.indexOf('@'));
@@ -17,14 +18,22 @@ Client.prototype.login = function(jid, pw) {
     );
 };
 
+Client.prototype.logout = function() {
+    this.con.disconnect();
+};
+
+// callbacks
 Client.prototype.connected = function() {
+    $('.jumbotron').empty();
+    $('.jumbotron').text("connected");
     console.log("connected");
+    this.con.send(new JSJaCPresence());
 };
 
 $("#login").submit(function(form) {
 
-    var jid = form.target.elements[0].value;
-    var pw  = form.target.elements[1].value
+    var jid = form.target.elements[0].value || 'zeank2@jwchat.org';
+    var pw  = form.target.elements[1].value || 'hkm18g';
 
     if (jid == '' || pw == '') {
         // show some error
@@ -33,7 +42,9 @@ $("#login").submit(function(form) {
         var client = new Client(config);
         client.login(jid, pw);
 
+        $(window).unload(function() { client.logout(); });
     }
 
     return false;
 });
+
